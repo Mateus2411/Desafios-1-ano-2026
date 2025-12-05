99af398b60712fb64bf831782fd0e37029b0f2e1
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2025-12-05 15:14:58 -0300

    bah

--- 05_Objetos/desafio3.js
+++ 05_Objetos/desafio3.js
@@ -1,1 +1,1 @@
-// Array de alunos (nome, sala, turma, idade, qual o ano que esle esta) min 10 alunos.
+// Array de alunos com objetos (nome, sala, turma, idade, qual o ano que esle esta) min 10 alunos.
